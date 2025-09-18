14c8ecf071794132afde70597f28ccb6addc2578
Orewa-veer <pratiharvirendra@gmail.com>
2025-09-18 13:58:52 +0530

    fix: fixed the main file

--- src/main.tsx
+++ src/main.tsx
@@ -3,25 +3,10 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.tsx";
 import "./index.css";
 import App from "./App.tsx";
-import { createSystem, defaultConfig, defineConfig } from "@chakra-ui/react";
-
-const config = defineConfig({
-  theme: {
-    tokens: {
-      colors: {
-        gray: {
-          50: "#f9f9f9",
-        },
-      },
-    },
-  },
-});
-
-const system = createSystem(defaultConfig, config);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider value={system}>
+    <Provider>
       <App />
     </Provider>
   </StrictMode>
